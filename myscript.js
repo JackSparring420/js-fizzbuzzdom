e16c69e58a59bff825d6eff22a4c37e21524ee32
JackSparring420 <emanuele.arconte94@gmail.com>
2021-10-25 15:52:56 +0200

    change card aspect ratio

--- myscript.js
+++ myscript.js
@@ -2,11 +2,11 @@ const containerCard = document.getElementById("containerCard")
 
 for (let i = 1; i<=1000; i++){
     if (i % 3 === 0 && i % 5 === 0) {
-        containerCard.innerHTML += `<div class= red> fizzbuzz </div>`
+        containerCard.innerHTML += `<div class= cardRed> fizzbuzz </div>`
     } else if (i % 5 === 0) {
-        containerCard.innerHTML += `<div class= yellow> buzz </div>`
+        containerCard.innerHTML += `<div class= cardYellow> buzz </div>`
     } else if (i % 3 === 0) {
-        containerCard.innerHTML += `<div class= green> fizz </div>`
+        containerCard.innerHTML += `<div class= cardGreen> fizz </div>`
     } else {
         containerCard.innerHTML += `<div class= card>${i}</div>`  
     }
