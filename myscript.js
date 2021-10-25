0a187bfafd8518ce8b639e0d1ffda81acff5c756
JackSparring420 <emanuele.arconte94@gmail.com>
2021-10-25 16:19:52 +0200

    completed exercide

--- myscript.js
+++ myscript.js
@@ -2,12 +2,12 @@ const containerCard = document.getElementById("containerCard")
 
 for (let i = 1; i<=1000; i++){
     if (i % 3 === 0 && i % 5 === 0) {
-        containerCard.innerHTML += `<div class= cardRed> fizzbuzz </div>`
+        containerCard.innerHTML += `<div class="card red"> fizzbuzz </div>`
     } else if (i % 5 === 0) {
-        containerCard.innerHTML += `<div class= cardYellow> buzz </div>`
+        containerCard.innerHTML += `<div class="card yellow"> buzz </div>`
     } else if (i % 3 === 0) {
-        containerCard.innerHTML += `<div class= cardGreen> fizz </div>`
+        containerCard.innerHTML += `<div class="card green"> fizz </div>`
     } else {
-        containerCard.innerHTML += `<div class= card>${i}</div>`  
+        containerCard.innerHTML += `<div class="card blue">${i}</div>`  
     }
 }
